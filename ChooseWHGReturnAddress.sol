06000261d181871260de49f9621c9d4cfd2e0c3c
Jordi Baylina <jordi@baylina.cat>
2017-07-23 22:28:32 +0200

    Scape hatch by the case

--- ChooseWHGReturnAddress.sol
+++ ChooseWHGReturnAddress.sol
@@ -1,6 +1,30 @@
 pragma solidity ^0.4.13;
 
-contract ChooseWHGReturnAddress {
+contract Owned {
+    function Owned() {
+        owner = msg.sender;
+    }
+
+    address public owner;
+
+    // This contract only defines a modifier and a few useful functions
+    // The function body is inserted where the special symbol "_" in the
+    // definition of a modifier appears.
+    modifier onlyOwner { if (msg.sender == owner) _; }
+
+    function changeOwner(address _newOwner) onlyOwner {
+        owner = _newOwner;
+    }
+
+    // This is a general safty function that allows the owner to do a lot
+    //  of things in the unlikely event that something goes wrong
+    // _dst is the contract being called making this like a 1/1 multisig
+    function execute(address _dst, uint _value, bytes _data) onlyOwner {
+        _dst.call.value(_value)(_data);
+    }
+}
+
+contract ChooseWHGReturnAddress is Owned {
 
     mapping (address => address) returnAddresses;
     uint public endDate;
